64262b9b0dec3d1d4d295dfe9b2cf7f29a545c38
Niklas Rindtorff <NiklasTR@users.noreply.github.com>
2024-04-15 14:45:53 +0200

    Update make_msa.sh

--- make_msa.sh
+++ make_msa.sh
@@ -20,14 +20,15 @@ DB_UR30="$PIPE_DIR/uniclust/UniRef30_2021_06"
 DB_BFD="$PIPE_DIR/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt"
 
 # Running signalP 6.0
-mkdir -p $out_dir/signalp
-tmp_dir="$out_dir/signalp"
-signalp6 --fastafile $in_fasta --organism other --output_dir $tmp_dir --format none --mode slow
-trim_fasta="$tmp_dir/processed_entries.fasta"
-if [ ! -s $trim_fasta ] # empty file -- no signal P
-then
-    trim_fasta="$in_fasta"
-fi
+#mkdir -p $out_dir/signalp
+#tmp_dir="$out_dir/signalp"
+#signalp6 --fastafile $in_fasta --organism other --output_dir $tmp_dir --format none --mode slow
+#trim_fasta="$tmp_dir/processed_entries.fasta"
+#if [ ! -s $trim_fasta ] # empty file -- no signal P
+#then
+#    trim_fasta="$in_fasta"
+#fi
+trim_fasta="$in_fasta"
 
 # setup hhblits command
 export HHLIB=/software/hhsuite/build/bin/
